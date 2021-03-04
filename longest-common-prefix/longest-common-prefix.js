ef8738523823b57fbc331b9964200825c626b621
Yan Tsishko <50244590+yantsishko@users.noreply.github.com>
2021-03-04 08:54:02 +0300

    Time: 80 ms (87.76%), Space: 38.9 MB (78.86%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+    let prefix = strs[0];
+    
+    for (let i = 1; i < strs.length; i += 1) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substr(0, prefix.length - 1);
+            if (prefix === "") return "";
+        }
+    }
+    
+    return prefix;
+};
