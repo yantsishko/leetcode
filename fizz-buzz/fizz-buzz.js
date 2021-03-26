c5c52bc8152f2d2204cd35a76ca04397769f188c
Yan Tsishko <50244590+yantsishko@users.noreply.github.com>
2021-03-26 09:14:42 +0300

    Time: 84 ms (74.78%), Space: 40.7 MB (64.06%) - LeetHub

--- fizz-buzz/fizz-buzz.js
+++ fizz-buzz/fizz-buzz.js
@@ -4,24 +4,79 @@
  */
 var fizzBuzz = function(n) {
     const result = [];
-    if (!n) return result;
+    
     for (let i = 1; i <= n; i++) {
-        const multiplesThree = i % 3 === 0;
-        const multiplesFive = i % 5 === 0;
-
-        if (multiplesThree && multiplesFive) {
+        const isThreeMultiplay = i % 3 === 0;
+        const isFiveMultiplay = i % 5 === 0;
+        if (isFiveMultiplay && isThreeMultiplay) {
             result.push('FizzBuzz');
-            continue;
-        } else if (multiplesThree) {
-           result.push('Fizz');
-            continue;
-        } else if (multiplesFive) {
-           result.push('Buzz');
-            continue;
+        } else if (isThreeMultiplay) {
+            result.push('Fizz');
+        } else if (isFiveMultiplay) {
+            result.push('Buzz');
         } else {
-            result.push(i.toString());
+            result.push(i + '');
         }
     }
     
     return result;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * @param {number} n
+//  * @return {string[]}
+//  */
+// var fizzBuzz = function(n) {
+//     const result = [];
+//     if (!n) return result;
+//     for (let i = 1; i <= n; i++) {
+//         const multiplesThree = i % 3 === 0;
+//         const multiplesFive = i % 5 === 0;
+
+//         if (multiplesThree && multiplesFive) {
+//             result.push('FizzBuzz');
+//             continue;
+//         } else if (multiplesThree) {
+//            result.push('Fizz');
+//             continue;
+//         } else if (multiplesFive) {
+//            result.push('Buzz');
+//             continue;
+//         } else {
+//             result.push(i.toString());
+//         }
+//     }
+    
+//     return result;
+// };
