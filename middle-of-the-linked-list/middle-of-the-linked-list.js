3f896904f499da766bd4198d499fa82d8339ff76
Yan Tsishko <50244590+yantsishko@users.noreply.github.com>
2021-03-31 09:25:31 +0300

    Time: 68 ms (95.57%), Space: 38.6 MB (16.18%) - LeetHub

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    return slow;
+};
