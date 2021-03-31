bd5d3ec4a26dd9ef8ed1805fc295d3ab2fc8d255
Yan Tsishko <50244590+yantsishko@users.noreply.github.com>
2021-03-31 09:28:33 +0300

    Time: 80 ms (68.01%), Space: 38.9 MB (61.75%) - LeetHub

---
+++ kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const max = Math.max(...candies);
+    
+    return candies.map((kid) => kid + extraCandies >= max)
+};
