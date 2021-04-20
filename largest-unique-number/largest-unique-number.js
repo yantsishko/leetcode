7dc9431c1063347098119abcad8686de25da36c7
Yan Tsishko <50244590+yantsishko@users.noreply.github.com>
2021-04-20 09:53:30 +0300

    Time: 80 ms (79.63%), Space: 41.2 MB (31.15%) - LeetHub

--- largest-unique-number/largest-unique-number.js
+++ largest-unique-number/largest-unique-number.js
@@ -4,18 +4,40 @@
  * T = O(N);
  * S = O(N)
  */
-var largestUniqueNumber = function(A) { 
-    const map = {};
+// var largestUniqueNumber = function(A) { 
+//     const map = {};
     
-    for (let i = 0; i < A.length; i++) {
-        map[A[i]] = map[A[i]] ? map[A[i]] + 1 : 1;
+//     for (let i = 0; i < A.length; i++) {
+//         map[A[i]] = map[A[i]] ? map[A[i]] + 1 : 1;
+//     }
+    
+//     const keys = Object.keys(map);
+    
+//     for (let i = keys.length - 1; i >= 0; i--) {
+//         if (map[keys[i]] === 1) return keys[i];
+//     }
+    
+//     return -1;
+// };
+
+const largestUniqueNumber = arr => {
+  const map = {};
+  
+  for (let i = 0; i < arr.length; i += 1) {
+    if (map[arr[i]]) {
+      map[arr[i]] += 1;
+    } else {
+      map[arr[i]] = 1;
     }
-    
-    const keys = Object.keys(map);
-    
-    for (let i = keys.length - 1; i >= 0; i--) {
-        if (map[keys[i]] === 1) return keys[i];
+  }
+  
+  let max = -1;
+  
+  Object.entries(map).forEach(([key, value]) => {
+    if (value === 1 && +key > max) {
+      max = key;
     }
-    
-    return -1;
-};
+  });
+  
+  return max;
+}
