12a364e4fa1c0e8bd55638e7062b137d5ff038c3
Yan Tsishko <50244590+yantsishko@users.noreply.github.com>
2021-06-22 19:29:37 +0300

    Time: 116 ms (11.73%), Space: 43 MB (5.56%) - LeetHub

---
+++ binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.js
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalOrder = function(root) {
+     /**
+                    3 (0)
+                /           \
+             9 (-1)         8 (1)
+            /       \      /     \
+        4 (-2)     0 (0) 1(0)    7 (2)
+                /           \
+            5 (-1)          2 (1)
+                
+      0 => [3, 0, 1]
+     -1 => [9, 5]
+      1 => 8, 2
+     -2 => 4
+      2 => 7
+      
+      T = O(N + MlogM)
+      S = O(N)
+     
+     **/
+    
+    if (!root) return [];
+
+    const queue = [{
+        node: root,
+        index: 0,
+    }];
+    
+    const result = {};
+
+    while (queue.length) {
+        const len = queue.length;
+        
+        for (let i = 0; i < len; i++) {
+            const currNode = queue.shift();
+            
+            if (!result[currNode.index]) {
+                result[currNode.index] = [currNode.node.val];
+            } else {
+                result[currNode.index].push(currNode.node.val);
+            }
+
+            if (currNode.node.left) {
+                queue.push({
+                    node: currNode.node.left,
+                    index: currNode.index - 1,
+                });
+            }
+
+            if (currNode.node.right) {
+                queue.push({
+                    node: currNode.node.right,
+                    index: currNode.index + 1,
+                });
+            }
+        }
+        
+    }
+    console.log(Object.entries(result).sort((a, b) => a[0] - b[0]));
+    return Object.entries(result).sort((a, b) => a[0] - b[0]).map(item => item[1]);
+    
+};
